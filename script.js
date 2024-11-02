d8f23e3ed77d42d31e79d2d7e4274e062b35c7a6
SzymonSauer <65712937+SzymonSauer@users.noreply.github.com>
2024-11-02 10:59:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,25 +18,3 @@ const checkInput = () => {
 // search.addEventListener('keydown', (event) => {
 // 	if (event.code === 'Enter') checkInput();
 // });
-
-/*
-search.addEventListener('keyup', checkInput);
-
-const search = document.querySelector('.search');
-const li = document.querySelectorAll('li');
-
-const searchEngine = (e) => {
-	const text = e.target.value.toLowerCase();
-	console.log(text);
-
-	li.forEach((x) => {
-		if (x.textContent.toLowerCase().indexOf(text) !== -1) {
-			x.style.display = 'block';
-		} else {
-			x.style.display = 'none';
-		}
-	});
-};
-
-search.addEventListener('keyup', searchEngine);
-*/
